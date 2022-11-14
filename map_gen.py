555f0c6be66a521a9930850ea36afefbe0592fdf
Sbug98 <68464158+Sbug98@users.noreply.github.com>
2022-11-14 21:20:35 +0100

    added source files

---
+++ map_gen.py
@@ -0,0 +1,32 @@
+def clean_hex(n):
+    return n.replace("0x", "").capitalize()
+
+def gen(name):
+    char_csv = open(name+".csv", "r")
+    char_map = char_csv.readlines()
+    char_csv.close()
+
+    res = '{'
+
+    for i in range(16):
+        line = char_map[i]
+        line_chars = line.split("\t")
+        res += '"'
+        first_hex_cipher = clean_hex(hex(i))+"-"
+        res += first_hex_cipher + '": {'
+        for j in range(16):
+            ascii_char = line_chars[j].replace("\n", "")
+            res += '"'
+            second_hex_cipher = "-"+clean_hex(hex(j))
+            if ascii_char == '"':
+                ascii_char = '\"'
+            res += second_hex_cipher+'": "'+ascii_char+'", '
+            if j==15:
+                res = res[0:len(res)-2]
+        res += "}, "
+
+    res = res[0:len(res)-2]+"}"
+    return res
+
+if __name__ == "__main__":
+    gen("FR")
---
+++ pk_char_to_ascii.py
@@ -0,0 +1,43 @@
+import map_gen
+import json
+
+modes = ["FR"]
+
+def main():
+    try:
+        selected = int(input("Select the charmap that you wnat to use (0->FireRed): "))
+    except:
+        print("Invalid number inserted")
+        return
+    
+    try:
+        char_map = json.loads(map_gen.gen(modes[selected]))
+        print(char_map)
+    except:
+        print("Could not load map char")
+
+    if  selected > len(modes)-1:
+        print("Invalid mode selected")
+        return
+
+    pkmn_char = input("Insert the Pokémon's name bytes in hex (10 bytes): ")
+    
+    if len(pkmn_char) != 20:
+        print("Invalid bytes inserted", len(pkmn_char))
+        return
+    
+    buf = ""
+    res = ""
+
+    for i in range(20):
+        if i%2 == 0:
+            buf = pkmn_char[i:i+2]
+            first = buf[0:1]
+            second = buf[1:2]
+            asciiChar = char_map[first.capitalize()+"-"]["-"+second.capitalize()]
+            res += asciiChar
+    
+    print("Result is: "+res)
+
+if __name__ == "__main__":
+    main()
