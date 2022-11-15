0273e701e003a1ac8624d8a27342dcd01e85c334
Sbug98 <68464158+Sbug98@users.noreply.github.com>
2022-11-15 10:05:55 +0100

    removed 10 bytes constraint, added some checks

--- pk_char_to_ascii.py
+++ pk_char_to_ascii.py
@@ -5,7 +5,7 @@ modes = ["FR"]
 
 def main():
     try:
-        selected = int(input("Select the charmap that you wnat to use (0->FireRed): "))
+        selected = int(input("Select the charmap that you want to use (0->FireRed): "))
     except:
         print("Invalid number inserted")
         return
@@ -13,27 +13,33 @@ def main():
     try:
         char_map = json.loads(map_gen.gen(modes[selected]))
     except:
-        print("Could not load map char")
+        print("Could not load char map")
+        return
 
     if  selected > len(modes)-1:
         print("Invalid mode selected")
         return
 
-    pkmn_char = input("Insert the Pokémon's name bytes in hex (10 bytes): ")
-    
-    if len(pkmn_char) != 20:
-        print("Invalid bytes inserted", len(pkmn_char))
+    try:
+        pkmn_char = input("Insert the Pokémon char bytes in hex: ")
+        int(pkmn_char, 16)
+    except:
+        print("Invalid hex value")
         return
     
+    if len(pkmn_char) % 2 != 0:
+        print("Invalid bytes")
+        return
+
     buf = ""
     res = ""
 
-    for i in range(20):
+    for i in range(len(pkmn_char)):
         if i%2 == 0:
             buf = pkmn_char[i:i+2]
             first = buf[0:1]
             second = buf[1:2]
-            asciiChar = char_map[first.capitalize()+"-"]["-"+second.capitalize()]
+            asciiChar = char_map[first.capitalize()][second.capitalize()]
             res += asciiChar
     
     print("Result is: "+res)
